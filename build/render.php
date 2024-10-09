aecba749929e683e42ab795a423cf58c2265dd35
Remon Pel <remon@acato.nl>
2024-10-09 08:33:08 +0200

    Ensure $src is actually a URL

--- build/render.php
+++ build/render.php
@@ -6,6 +6,11 @@
 if ( ! empty( $attributes['shortcode'] ) ) :
 	preg_match( '/src="([^"]+)"/', $attributes['shortcode'], $matches );
 	$src = ( isset( $matches[1] ) ) ? esc_url( $matches[1] ) : $attributes['shortcode'];
+
+	// Validate that $src is in fact a URL.
+	if ( ! filter_var( $src, FILTER_VALIDATE_URL ) ) {
+		return;
+	}
 ?>
 
 <div <?php echo get_block_wrapper_attributes(); ?>>
