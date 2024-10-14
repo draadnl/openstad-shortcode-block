01c53e330f4dc8898d4da01716263597b823133c
Chris Schotman <chris@draad.nl>
2024-10-14 10:14:05 +0200

    Improved editor.scss

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '021bd6454440816d1c25');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '07d1ea8ac9b89d1646aa');
